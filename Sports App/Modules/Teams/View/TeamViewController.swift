4416540ad78276ef019d7e4dec69975873a6d1ae
nada <nadaayman9997@gmail.com>
2023-05-23 03:44:47 +0300

    add Teams Details design

---
+++ Sports App/Modules/Teams/View/TeamViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TeamViewController.swift
+//  Sports App
+//
+//  Created by Nada on 23/05/2023.
+//
+
+import UIKit
+
+class TeamViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ Sports App/Modules/Teams/ViewModel/TeamViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  TeamViewModel.swift
+//  Sports App
+//
+//  Created by Nada on 23/05/2023.
+//
+
+import Foundation
